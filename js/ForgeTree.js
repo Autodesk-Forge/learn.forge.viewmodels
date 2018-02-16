4cea57db39b136c08a6d33fce70fadb017e160a2
Augusto Goncalves <augusto.goncalves@autodesk.com>
2018-02-16 09:57:12 -0200

    UI for forge.learning.viewmodels tutorial

---
+++ js/ForgeTree.js
@@ -0,0 +1,133 @@
+﻿$(document).ready(function () {
+  prepareAppBucketTree();
+  $('#refreshBuckets').click(function () {
+    $('#appBuckets').jstree(true).refresh();
+  });
+
+  $('#createNewBucket').click(function () {
+    createNewBucket();
+  });
+
+  $('#createBucketModal').on('shown.bs.modal', function () {
+    $("#newBucketKey").focus();
+  })
+});
+
+function createNewBucket() {
+  var bucketKey = $('#newBucketKey').val();
+  var policyKey = $('#newBucketPolicyKey').val();
+  jQuery.post({
+    url: '/api/forge/oss/buckets',
+    data: { 'bucketKey': bucketKey, 'policyKey': policyKey },
+    success: function (res) {
+      $('#appBuckets').jstree(true).refresh();
+      $('#createBucketModal').modal('toggle');
+    },
+  });
+}
+
+function prepareAppBucketTree() {
+  $('#appBuckets').jstree({
+    'core': {
+      'themes': { "icons": true },
+      'data': {
+        "url": '/api/forge/oss/buckets',
+        "dataType": "json",
+        'multiple': false,
+        "data": function (node) {
+          return { "id": node.id };
+        }
+      }
+    },
+    'types': {
+      'default': {
+        'icon': 'glyphicon glyphicon-question-sign'
+      },
+      '#': {
+        'icon': 'glyphicon glyphicon-cloud'
+      },
+      'bucket': {
+        'icon': 'glyphicon glyphicon-folder-open'
+      },
+      'object': {
+        'icon': 'glyphicon glyphicon-file'
+      }
+    },
+    "plugins": ["types", "state", "sort", "contextmenu"],
+    contextmenu: { items: autodeskCustomMenu }
+  }).on('loaded.jstree', function () {
+    $('#appBuckets').jstree('open_all');
+  }).bind("activate_node.jstree", function (evt, data) {
+    if (data != null && data.node != null && data.node.type == 'object') {
+      launchViewer(data.node.id);
+    }
+  });
+}
+
+function autodeskCustomMenu(autodeskNode) {
+  var items;
+
+  switch (autodeskNode.type) {
+    case "bucket":
+      items = {
+        uploadFile: {
+          label: "Upload file",
+          action: function () {
+            var treeNode = $('#appBuckets').jstree(true).get_selected(true)[0];
+            uploadFile(treeNode);
+          }
+        }
+      };
+      break;
+    case "object":
+      items = {
+        translateFile: {
+          label: "Translate",
+          action: function () {
+            var treeNode = $('#appBuckets').jstree(true).get_selected(true)[0];
+            translateObject(treeNode);
+          }
+        }
+      };
+      break;
+  }
+
+  return items;
+}
+
+function uploadFile(node) {
+  $('#hiddenUploadField').click();
+  $('#hiddenUploadField').change(function () {
+    var file = this.files[0];
+    switch (node.type) {
+      case 'bucket':
+        var formData = new FormData();
+        formData.append('fileToUpload', file);
+        formData.append('bucketKey', node.id);
+
+        $.ajax({
+          url: '/api/forge/oss/objects',
+          data: formData,
+          processData: false,
+          contentType: false,
+          type: 'POST',
+          success: function (data) {
+            $('#appBuckets').jstree(true).refresh_node(node);
+          }
+        });
+        break;
+    }
+  });
+}
+
+function translateObject(node) {
+  var bucketKey = node.parents[0];
+  var objectKey = node.id;
+  jQuery.post({
+    url: '/api/forge/modelderivative/jobs',
+    data: { 'bucketKey': bucketKey, 'objectName': objectKey },
+    success: function (res) {
+
+    },
+  });
+}
