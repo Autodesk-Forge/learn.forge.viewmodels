d288f120b082554bfa9f3e96875772f003038ea4
Denis Grigor <apprentice3d@gmail.com>
2018-03-06 08:36:33 -0500

    preliminary setup of go approach.

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/apprentice3d/forge.learning.viewmodels.go/server"
+)
+
+const (
+	PORT = ":3000"
+)
+
+func main() {
+
+	clientID := os.Getenv("FORGE_CLIENT_ID")
+	clientSecret := os.Getenv("FORGE_CLIENT_SECRET")
+
+	if clientID == "" || clientSecret == "" {
+		log.Fatal("The FORGE_CLIENT_ID and FORGE_CLIENT_SECRET env vars are not set. \nExiting ...")
+	}
+
+	log.Println("Starting server on port ", PORT)
+	server.StartServer(PORT, clientID, clientSecret)
+}
