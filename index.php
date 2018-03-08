a52ac470093079dca629bb45be627b6a03710fb8
Zhong Wu <wuzhong1980@gmail.com>
2018-03-08 22:07:30 +0800

    update php sample

--- index.php
+++ index.php
@@ -1,41 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zhongwu
- * Date: 2018/3/6
- * Time: 下午3:30
- */
 
 include_once "./server/vendor/autoload.php";
 include_once "./server/apis.php";
 
-$klein = new \Klein\Klein();
+use Klein\Klein;
+use Autodesk\Controller\ForgeServices;
 
-$klein->respond('GET', '/', function () {
-    $c = new \server\Controller\Demo();
-    return $c->test();
+$klein = new Klein();
+
+// Get the access token
+$klein->respond('GET', '/api/forge/oauth/token', function () {
+    $forgeServices = new ForgeServices();
+    return $forgeServices->getAccessToken();
 });
 
+// Get all the buckets
 $klein->respond('GET', '/api/forge/oss/buckets', function () {
-    $c = new \server\Controller\Demo();
-    return $c->getAllBuckets();
+    $forgeServices = new ForgeServices();
+    return $forgeServices->getAllBuckets();
 });
 
+// Create a new bucket
 $klein->respond('POST', '/api/forge/oss/buckets', function(){
-    $c = new \server\Controller\Demo();
-    return $c->createOneBucket();
-
+    $forgeServices = new ForgeServices();
+    return $forgeServices->createOneBucket();
 });
 
-$klein->respond('GET', '/api/token', function () {
-    $c = new \server\Controller\Demo();
-    return $c->getAccessToken();
+// Upload a file to a bucket
+$klein->respond('POST', '/api/forge/oss/objects', function () {
+    $forgeServices = new ForgeServices();
+    return $forgeServices->uploadFile();
 });
 
-$klein->respond('POST', '/api/token', function () {
-    $c = new \server\Controller\Demo();
-    return $c->test();
+// Start translate the model
+$klein->respond('POST', '/api/forge/modelderivative/jobs', function () {
+    $forgeServices = new ForgeServices();
+    return $forgeServices->translateFile();
 });
+
 
 $klein->dispatch();
 
--- server/apis.php
+++ server/apis.php
@@ -1,54 +1,63 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zhongwu
- * Date: 2018/3/6
- * Time: 下午3:55
- */
-namespace server\Controller;
+
+namespace Autodesk\Controller;
 use Autodesk\Auth\Configuration;
 use Autodesk\Auth\OAuth2\TwoLeggedAuth;
 
-
 use Autodesk\Forge\Client\Api\BucketsApi;
 use Autodesk\Forge\Client\Model\PostBucketsPayload;
-// use Autodesk\Forge\Client\Model\JobIgesOutputPayload;
 
-class Demo
+use Autodesk\Forge\Client\Api\ObjectsApi;
+
+use Autodesk\Forge\Client\Model\JobPayload;
+use Autodesk\Forge\Client\Model\JobPayloadInput;
+use Autodesk\Forge\Client\Model\JobPayloadOutput;
+use Autodesk\Forge\Client\Model\JobPayloadItem;
+use Autodesk\Forge\Client\Api\DerivativesApi;
+
+class ForgeServices
 {
     const CLIENTID = 'z8fGD7sOz27dGsiG1g3df2nelkXchTAV';
     const CLIENTSECRET = '7BxtA1m7Y3NAuhPv';
     private $twoLeggedAuth;
 
-
-
-    public function getAccessToken()
+    public function __construct()
     {
-     
+        set_time_limit(0);
+
         Configuration::getDefaultConfiguration()
             ->setClientId(self::CLIENTID)
             ->setClientSecret(self::CLIENTSECRET);
         $this->twoLeggedAuth = new TwoLeggedAuth();
-        $this->twoLeggedAuth->setScopes(['bucket:read']);
+        $this->twoLeggedAuth->setScopes(['bucket:read','data:read','bucket:create','data:create','data:write','account:read','account:write']);
         $this->twoLeggedAuth->fetchToken();
-        $tokenInfo = [
-            'applicationToken' => $this->twoLeggedAuth->getAccessToken(),
-            'expiry'           => time() + $this->twoLeggedAuth->getExpiresIn(),
-        ];
-        // echo json_encode($tokenInfo);
+    }    
+
+    public function getAccessToken()
+    {
+        $tokenInfo = array(
+            'access_token'  => $this->twoLeggedAuth->getAccessToken(),
+            'expires_in'    => $this->twoLeggedAuth->getExpiresIn(),
+        );
+        echo json_encode($tokenInfo);
     }
 
 
     public function createOneBucket(){
-        // print_r($_POST);
-        // print_r($_GET);
+        // get the request body
+        $body = json_decode(file_get_contents('php://input', 'r'), true);
         
-        $this->getAccessToken();
+        $bucketKey = $body['bucketKey'];
+        // $policeKey = $body['policyKey'];
+        $policeKey = "transient";
+
         $apiInstance = new BucketsApi($this->twoLeggedAuth);
-        $post_buckets = new PostBucketsPayload(); 
+        $post_bucket = new PostBucketsPayload(); 
+        $post_bucket->setBucketKey($bucketKey);
+        $post_bucket->setPolicyKey($policeKey);
 
         try {
-            $result = $apiInstance->createBucket($post_buckets, $x_ads_region);
+            $result = $apiInstance->createBucket($post_bucket);
             print_r($result);
         } catch (Exception $e) {
             echo 'Exception when calling BucketsApi->createBucket: ', $e->getMessage(), PHP_EOL;
@@ -59,24 +68,116 @@ class Demo
      /////////////////////////////////////////////////////////////////////////
      public function getAllBuckets()
      {
-        //  echo  'getAllBuckets';
-         $this->getAccessToken();
-         $apiInstance = new BucketsApi($this->twoLeggedAuth);
-         try {
-             $result = $apiInstance->getBuckets();
-            //  print_r($result);
-             echo $result;
-             die; 
-         } catch (Exception $e) {
-             echo 'Exception when calling BucketsApi->getBuckets: ', $e->getMessage(), PHP_EOL;
-         }
+        $id = $_GET['id'];
+        if ($id === '#') {// root
+            $apiInstance = new BucketsApi($this->twoLeggedAuth);
+            try{
+                $result = $apiInstance->getBuckets();
+                $resultArray = json_decode($result, true);
+                $buckets = $resultArray['items'];
+                $bucketsLength = count($buckets);
+                $bucketlist = array();
+                for($i=0; $i< $bucketsLength; $i++){
+                    $bucketInfo = array('id'=>$buckets[$i]['bucketKey'],
+                                        'text'=>$buckets[$i]['bucketKey'],
+                                        'type'=>'bucket',
+                                        'children'=>true
+                    );
+                    array_push($bucketlist, $bucketInfo);
+                }
+                echo json_encode($bucketlist);
+                die; 
+            }catch (Exception $e) {
+                echo 'Exception when calling BucketsApi->getBuckets: ', $e->getMessage(), PHP_EOL;
+            }
+        }
+        else{
+            $apiInstance = new ObjectsApi($this->twoLeggedAuth);
+            $bucket_key = $id; 
+            try {
+                $result = $apiInstance->getObjects($bucket_key);
+                $resultArray = json_decode($result, true);
+                $objects = $resultArray['items'];
+
+                $objectsLength = count($objects);
+                $objectlist = array();
+                for($i=0; $i< $objectsLength; $i++){
+                    
+                    $objectInfo = array('id'=>base64_encode($objects[$i]['objectId']),
+                                        'text'=>$objects[$i]['objectKey'],
+                                        'type'=>'object',
+                                        'children'=>false
+                    );
+                    array_push($objectlist, $objectInfo);
+                }
+                echo json_encode($objectlist);
+                die; 
+            } catch (Exception $e) {
+                echo 'Exception when calling ObjectsApi->getObjects: ', $e->getMessage(), PHP_EOL;
+            }
+        }
      }
  
 
-    public function test()
-    {
-        //print_r($_SERVER);
-        print_r($_POST);
-        print_r($_GET);
+    public function translateFile(){
+        $body = json_decode(file_get_contents('php://input', 'r'), true);
+        $objectId = $body['objectName'];
+
+        $apiInstance = new DerivativesApi($this->twoLeggedAuth);
+        $job         = new JobPayload(); 
+
+        $jobInput    = new JobPayloadInput();
+        $jobInput->setUrn($objectId);
+
+        $jobOutputItem = new JobPayloadItem();
+        $jobOutputItem->setType('svf');
+        $jobOutputItem->setViews(array('2d','3d'));
+        
+        $jobOutput   = new JobPayloadOutput();
+        $jobOutput->setFormats(array($jobOutputItem));
+
+        $job->setInput($jobInput);
+        $job->setOutput($jobOutput);
+
+        $x_ads_force = false; 
+        try {
+            $result = $apiInstance->translate($job, $x_ads_force);
+            print_r($result);
+        } catch (Exception $e) {
+            echo 'Exception when calling DerivativesApi->translate: ', $e->getMessage(), PHP_EOL;
+        }
     }
+
+
+    public function uploadFile()
+    {
+        // $apiInstance 
+        // $body = file_get_contents('php://input', 'r');
+        // var_dump($body);
+        
+        // var_dump(($_POST));
+        // die;
+
+        $body = $_POST;
+        $file = $_FILES;
+        // $_SESSION['file'] = $file;
+        // var_dump($_SESSION['file']);die;
+        // var_dump($_FILES);die;
+        // die;
+
+        $apiInstance = new ObjectsApi($this->twoLeggedAuth);
+        $bucket_key  = $body['bucketKey']; 
+        $fileToUpload    = $file['fileToUpload'];
+        $filePath = $fileToUpload['tmp_name'];
+        $content_length = filesize($filePath); 
+
+        // $fileRead = fread($filePath, $content_length);
+        
+        try {
+            $result = $apiInstance->uploadObject($bucket_key, $fileToUpload['name'], $content_length, $filePath );
+            print_r($result);
+        } catch (Exception $e) {
+            echo 'Exception when calling ObjectsApi->uploadObject: ', $e->getMessage(), PHP_EOL;
+        }
+    }  
 }
