b72896ce6861b63b8933ad585bdbda9370768669
Zhong Wu <wuzhong1980@gmail.com>
2018-03-17 14:48:21 +0800

    update

--- server/config.php
+++ server/config.php
@@ -5,11 +5,11 @@ namespace Autodesk\ForgeServices;
 
 class ForgeConfig{
 
-    public static $forge_id     = "<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>";
-    public static $forge_secret = "<<YOUR CLIENT SECRET>>";
-
-    // public static $forge_id      = getenv("FORGE_CLIENT_ID");
-    // public static $forge_secret  = getenv("FROGE_CLIENT_SECRET");
+    public static $forge_id     = "z8fGD7sOz27dGsiG1g3df2nelkXchTAV";
+    public static $forge_secret = "7BxtA1m7Y3NAuhPv";
+    
+    // public static $forge_id     = "<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>";
+    // public static $forge_secret = "<<YOUR CLIENT SECRET>>";
 
   // Required scopes for your application on server-side
   public static $scopeInternal = ['bucket:create', 'bucket:read', 'data:read', 'data:create', 'data:write'];
